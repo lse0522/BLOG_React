74bfd48ba006e9c2bb6b961991023c831ca00128
se eun <88942643+lse0522@users.noreply.github.com>
2022-08-18 14:52:25 +0900

    #1.완성

--- src/App.js
+++ src/App.js
@@ -2,30 +2,35 @@ import './App.css';
 import {useState} from 'react';
 
 function App() {
-  let [글제목, 글제목변경] = useState('');
+  //let [글제목, 글제목변경] = useState( ['남자코트 추천', '강남 우동맛집', '파이썬 독학'] );
+  let [글제목, 글제목변경] = useState([]);
   let [title, titlechange] = useState('');
 
   return (
     <div className="App">
       <input type="text" onChange={(e)=>{
-        글제목변경(e.target.value);
+        titlechange(e.target.value);
       }} />
       <button type="sumbit" onClick={()=>{
         let copy글제목=[...글제목];
-        titlechange(copy글제목);
+        copy글제목.unshift(title);
+        글제목변경(copy글제목);
       }}>글발행</button>
 
-
-      <div className="list">
-        <h4>{title}</h4>
-        <p>2022.08.18</p>
-      </div>
+    {
+      글제목.map(function(a, i){
+        return(
+          <div className="list" key={i}>
+            <h4>{ 글제목[i] }</h4>
+            <p>2022.08.18</p>
+          </div>
+        )
+      })
+    }
 
     </div>
   );
 }
-
-
 
 
 export default App;
