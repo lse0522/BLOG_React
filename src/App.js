7d7f3bde12cb0ba8aeb0860640e68d6a182183b1
se eun <88942643+lse0522@users.noreply.github.com>
2022-08-18 13:58:25 +0900

    #1. input&button
    input+button으로 글 제목 받아오기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import './App.css';
+import {useState} from 'react';
 
 function App() {
+  let [글제목, 글제목변경] = useState('');
+  let [title, titlechange] = useState('');
 
   return (
     <div className="App">
- 
+      <input type="text" onChange={(e)=>{
+        글제목변경(e.target.value);
+      }} />
+      <button type="sumbit" onClick={()=>{
+        let copy글제목=[...글제목];
+        titlechange(copy글제목);
+      }}>글발행</button>
+
+
+      <div className="list">
+        <h4>{title}</h4>
+        <p>2022.08.18</p>
+      </div>
 
     </div>
   );
